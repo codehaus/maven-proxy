d2e18bd4ccc1172815ef30e213658f7db0e6ffd6
bwalding <bwalding@4a46e9b2-df14-0410-8909-ca38c606e0b8>
2003-08-19 22:07:27 +0000

    *** empty log message ***

---
+++ maven-proxy/core/src/main/java/org/apache/maven/proxy/RetrievalConfiguration.java
@@ -0,0 +1,12 @@
+package org.apache.maven.proxy;
+
+/**
+ * This is where all configuration elements for a single
+ * retrieval is to go.
+ * @author  Ben Walding
+ * @version $Id$
+ */
+public class RetrievalConfiguration
+{
+
+}
