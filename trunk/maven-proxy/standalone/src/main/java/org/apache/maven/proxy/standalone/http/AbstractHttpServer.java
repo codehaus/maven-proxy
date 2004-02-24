9d9098e3b9a52d0eba5d9f346912f1d5c093f4e9
bwalding <bwalding@4a46e9b2-df14-0410-8909-ca38c606e0b8>
2004-02-24 01:18:54 +0000

    This is all a load of shite, committing then backing out all the custom http server.
    Jetty is fine.  And jetty-experimental if we need to go even more cut down.

--- trunk/maven-proxy/standalone/src/main/java/org/apache/maven/proxy/standalone/http/AbstractHttpServer.java
+++ trunk/maven-proxy/standalone/src/main/java/org/apache/maven/proxy/standalone/http/AbstractHttpServer.java
@@ -1,5 +1,21 @@
 package org.apache.maven.proxy.standalone.http;
 
+/*
+ * Copyright 2003-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -11,6 +27,7 @@ import java.net.Socket;
 
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.maven.proxy.ResourceUtil;
 
 /**
  * AbstractHttpServer is an abstract class that provides the
@@ -107,13 +124,7 @@ public abstract class AbstractHttpServer implements Runnable, HttpServer
 		}
 		finally
 		{
-			try
-			{
-				socket.close();
-			}
-			catch (IOException e)
-			{
-			}
+			ResourceUtil.close(socket);
 		}
 	}
 
--- trunk/maven-proxy/standalone/src/test/java/org/apache/maven/proxy/standalone/http/HttpServerTest.java
+++ trunk/maven-proxy/standalone/src/test/java/org/apache/maven/proxy/standalone/http/HttpServerTest.java
@@ -1,15 +1,24 @@
-/*
- * Created on 21/10/2003
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
 package org.apache.maven.proxy.standalone.http;
+
+/*
+ * Copyright 2003-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import junit.framework.TestCase;
 
 import org.apache.avalon.framework.configuration.DefaultConfiguration;
-import org.apache.maven.proxy.standalone.ProxyRepoServer;
 
 /**
  * @author  Ben Walding
@@ -17,16 +26,11 @@ import org.apache.maven.proxy.standalone.ProxyRepoServer;
  */
 public class HttpServerTest extends TestCase
 {
-	public void testX()
-	{
-
-	}
-
 	public void testRun() throws Exception
 	{
 		AbstractHttpServer server = new MockHttpServer();
 		DefaultConfiguration c = new DefaultConfiguration("test");
-		c.addAttribute("port", "9999");
+        c.addAttribute("port", "9999");
 		server.configure(c);
 		server.start();
 		Thread.sleep(60000);
