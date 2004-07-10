9eb87b8b20e2ea5b69000bdc6ffd04e17c3588d0
bwalding <bwalding@4a46e9b2-df14-0410-8909-ca38c606e0b8>
2004-07-10 02:21:43 +0000

    all modules will build a snapshot with the build:snapshot goal

--- maven-proxy/build.sh
+++ maven-proxy/build.sh
@@ -5,7 +5,7 @@ echo Building maven-proxy
 for module in core standalone webapp;
 do
   cd $module
-  maven 
+  maven build:snapshot
   RESULT=$?
   if [ $RESULT -ne "0" ]; then
     echo Exit value of $RESULT in module $module
